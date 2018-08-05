ee9343c28ccf45178f454556e2778ba2eed9692c
Kevin Hennessy <khenness@tcd.ie>
2018-08-05 20:34:45 +0100

    2d transformation work

--- matrix_multiplication_example.py
+++ matrix_multiplication_example.py
@@ -19,3 +19,33 @@ print(np.dot(x, v))
 #  [43 50]]
 print(x.dot(y))
 print(np.dot(x, y))
+
+
+print("kevins stuff:")
+#video time 13.45
+m1 = np.array([[1,0,4],
+               [0,1,-3],
+               [0,0,1]])
+
+m2 = np.array([[2],
+               [6],
+               [1]])
+
+print(np.dot(m1,m2))
+
+
+
+def translate(x,y, a,b):
+    #x and y are the point to be translated, a and b are the translations in the x direction and y direction
+    # video time 13.45
+    m1 = np.array([[1, 0, a],
+                   [0, 1, b],
+                   [0, 0, 1]])
+    m2 = np.array([[x],
+                   [y],
+                   [1]])
+
+    return(np.dot(m1, m2))
+
+
+print("translate(2,6,4,-3) =\n"+str(translate(2,6,4,-3)))
